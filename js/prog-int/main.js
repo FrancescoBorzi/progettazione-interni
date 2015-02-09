fe8a24770fae90b62ace139df879272055d9fa7f
Helias <stefanoborzi32@gmail.com>
2015-02-10 00:43:50 +0100

    Aggiornata pagina progetti

--- js/prog-int/main.js
+++ js/prog-int/main.js
@@ -86,17 +86,17 @@
 
   app.controller("ProjectController", function($scope){
     this.projects = [
-      { src: "img/progetti/miniature/11_7-150x150.jpg", desc: "Prog. Interni: ristrutturazioni."},
-      { src: "img/progetti/miniature/12_16-150x150.jpg", desc: "Prog. Interni: arredamento."},
-      { src: "img/progetti/miniature/24_23-150x150.jpg", desc: "Prog. Interni: ex novo."},
-      { src: "img/progetti/miniature/25_6-150x150.jpg", desc: "Prog. Interni: soluzioni diverse…"},
-      { src: "img/progetti/miniature/26_27-150x150.jpg", desc: "Prog. Interni: nuova distribuzione…"},
-      { src: "img/progetti/miniature/Immagine-1.-_Stima-di-un-immobile_-150x150.jpg", desc: "Progetti recenti: stima di un immobile"},
-      { src: "img/progetti/miniature/Immagine-2.-_Sfruttato-al-massimo_-150x150.jpg", desc: "Progetti recenti: sfruttato al massimo."},
-      { src: "img/progetti/miniature/Immagine-3.-_Abitare-in-campagna_-150x150.jpg", desc: "Progetti recenti: abitare in campagna."},
-      { src: "img/progetti/miniature/Immagine-4.-_Appartamento-mare-monti_-150x150.jpg", desc: "Progetti recenti: appartamenti mari e monti."},
-      { src: "img/progetti/miniature/Immagine-5.-_Vivere-in-mansarda_-150x150.jpg", desc: "Progetti recenti: vivere in mansarda."},
-      { src: "img/progetti/miniature/Immagine-6.-_Lingresso-aperto_-150x150.jpg", desc: "Progetti recenti: l’ingresso aperto."}
+      { src: "11_7-150x150.jpg", desc: "Prog. Interni: ristrutturazioni."},
+      { src: "12_16-150x150.jpg", desc: "Prog. Interni: arredamento."},
+      { src: "24_23-150x150.jpg", desc: "Prog. Interni: ex novo."},
+      { src: "25_6-150x150.jpg", desc: "Prog. Interni: soluzioni diverse…"},
+      { src: "26_27-150x150.jpg", desc: "Prog. Interni: nuova distribuzione…"},
+      { src: "Immagine-1.-_Stima-di-un-immobile_-150x150.jpg", desc: "Progetti recenti: stima di un immobile"},
+      { src: "Immagine-2.-_Sfruttato-al-massimo_-150x150.jpg", desc: "Progetti recenti: sfruttato al massimo."},
+      { src: "Immagine-3.-_Abitare-in-campagna_-150x150.jpg", desc: "Progetti recenti: abitare in campagna."},
+      { src: "Immagine-4.-_Appartamento-mare-monti_-150x150.jpg", desc: "Progetti recenti: appartamenti mari e monti."},
+      { src: "Immagine-5.-_Vivere-in-mansarda_-150x150.jpg", desc: "Progetti recenti: vivere in mansarda."},
+      { src: "Immagine-6.-_Lingresso-aperto_-150x150.jpg", desc: "Progetti recenti: l’ingresso aperto."}
     ];
   });
 })()
