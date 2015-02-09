46fa0ebc7259ab3b080068ad8dd63dfd114047d7
ShinDarth <borzifrancesco@gmail.com>
2015-02-09 21:11:18 +0100

    configurazione states

--- js/prog-int/main.js
+++ js/prog-int/main.js
@@ -1,3 +1,53 @@
 (function() {
   var app = angular.module('prog-int', ['ui.router', 'ui.bootstrap']);
+
+  app.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise("/home");
+
+    $stateProvider
+    .state('home', {
+      url: '/home',
+      views: {
+        '': {
+          templateUrl: 'partials/home.html'
+        },
+        'home-bottom': {
+          templateUrl: 'partials/home-bottom.html'
+        }
+      }
+    })
+    .state('chi-siamo', {
+      url: '/chi-siamo',
+      views: {
+        '': {
+          templateUrl: 'partials/chi-siamo.html'
+        }
+      }
+    })
+    .state('realizzazione-villette', {
+      url: '/realizzazione-villette',
+      views: {
+        '': {
+          templateUrl: 'partials/realizzazione-villette.html'
+        }
+      }
+    })
+    .state('progetti', {
+      url: '/progetti',
+      views: {
+        '': {
+          templateUrl: 'partials/progetti.html'
+        }
+      }
+    })
+    .state('contatti', {
+      url: '/contatti',
+      views: {
+        '': {
+          templateUrl: 'partials/contatti.html'
+        }
+      }
+    })
+  });
 })()
